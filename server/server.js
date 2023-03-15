2ef7dea6b7d47d115db920d3e727da2288f57a7e
roddazad <roddazad@gmail.com>
2023-03-14 20:43:31 -0700

    depoy

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const {
 const http = require("http");
 const { typeDefs, resolvers } = require("./schemas");
 const db = require('./config/connection');
-
+const path = require('path');
 const PORT = process.env.PORT || 3001;
 const app = express();
 const httpServer = http.createServer(app);
